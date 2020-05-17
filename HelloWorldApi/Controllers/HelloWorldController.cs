1266ca63dc620b7b994f278008d19cb22f5ec1ac
pedrozamarioli <44202350+pedrozamarioli@users.noreply.github.com>
2020-05-17 14:11:53 +0100

    first commit

---
+++ HelloWorldApi/Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var localIpAddress = HttpContext.Connection.LocalIpAddress;
+            var localPort = HttpContext.Connection.LocalPort;
+            return $"Hello World\nIp address: {localIpAddress}\nPort: {localPort}";
+        }
+    }
+}
---
+++ HelloWorldApi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace HelloWorldApi
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+            var host = CreateWebHostBuilder(args).Build();
+            await host.RunAsync(cts.Token);
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
